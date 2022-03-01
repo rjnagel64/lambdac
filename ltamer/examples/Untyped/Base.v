32092e28756e59c7e5850943e6bf5224774476a0
Robert J Nagel <rjnagel64@gmail.com>
2022-03-01 12:06:36 -0600

    Don't commit ltamer library

--- ltamer/examples/Untyped/Base.v
+++
@@ -1,6 +0,0 @@
-Parameter base : Set.
-Parameter encode : base -> nat.
-
-Axiom encode_inj : forall x y, encode x = encode y -> x = y.
-
-Parameter base_eq_dec : forall x y : base, {x = y} + {x <> y}.
