3d0ebb233ec9bb019fe4665340a0db54edd7285b
Robert J Nagel <rjnagel64@gmail.com>
2022-03-01 10:32:11 -0600

    Parse command line options and read from files

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,9 @@
 
 module Main where
 
+import Control.Monad (when)
 import System.Exit
+import Options.Applicative
 
 import qualified Lexer as L
 import qualified Parser as P
@@ -17,6 +19,8 @@ import qualified Emit as E
 
 -- Note: Returning multiple values from a function is passing multiple values
 -- to the continuation.
+-- Note: Returning a choice of values from a function is selecting which
+-- continuation to invoke.
 
 parseString :: String -> IO S.Term
 parseString s = case P.parseTerm (L.lex s) of
@@ -27,29 +31,55 @@ parseString s = case P.parseTerm (L.lex s) of
 parseFile :: FilePath -> IO S.Term
 parseFile f = readFile f >>= parseString
 
-src :: String
-src = "let fun f x = case iszero x of { inl z -> 0; inr z -> x + f (x + -1) }; in f 10"
+-- src = "let fun f x = case iszero x of { inl z -> 0; inr z -> x + f (x + -1) }; in f 10"
+-- src = "\\x -> x"
 -- src = "2 + 2"
+
+data DriverArgs
+  = DriverArgs {
+    driverFile :: String
+  , driverDumpCPS :: Bool
+  , driverDumpCC :: Bool
+  , driverDumpHoist :: Bool
+  , driverDumpEmit :: Bool
+  }
+
+driver :: Parser DriverArgs
+driver = DriverArgs
+  <$> argument str (metavar "FILE")
+  <*> switch (long "dump-cps" <> help "whether to dump CPS IR")
+  <*> switch (long "dump-cc" <> help "whether to dump CC IR")
+  <*> switch (long "dump-hoist" <> help "whether to dump Hoist IR")
+  <*> switch (long "dump-emit" <> help "whether to dump Emit C output")
+
+opts :: ParserInfo DriverArgs
+opts = info (helper <*> driver) (fullDesc <> progDesc "Compile LambdaC")
 
 main :: IO ()
 main = do
-  srcS <- parseString src
+  args <- execParser opts
 
-  putStrLn $ "--- CPS Transform ---"
+  srcS <- parseFile (driverFile args)
+
   let srcK = K.cpsMain srcS
-  putStrLn $ K.pprintTerm 0 srcK
+  when (driverDumpCPS args) $ do
+    putStrLn $ "--- CPS Transform ---"
+    putStrLn $ K.pprintTerm 0 srcK
 
-  putStrLn $ "--- Closure Conversion ---"
   let srcC = C.cconv srcK
-  putStrLn $ C.pprintTerm 0 srcC
+  when (driverDumpCC args) $ do
+    putStrLn $ "--- Closure Conversion ---"
+    putStrLn $ C.pprintTerm 0 srcC
 
-  putStrLn $ "--- Hoisting ---"
   let (srcH, decls) = H.runHoist $ H.hoist srcC
-  putStrLn $ concatMap H.pprintTop decls ++ H.pprintTerm 0 srcH
+  when (driverDumpHoist args) $ do
+    putStrLn $ "--- Hoisting ---"
+    putStrLn $ concatMap H.pprintTop decls ++ H.pprintTerm 0 srcH
 
-  putStrLn $ "--- Code Generation ---"
   let obj = unlines $ E.emitProgram (decls, srcH)
-  putStrLn obj
+  when (driverDumpEmit args) $ do
+    putStrLn $ "--- Code Generation ---"
+    putStrLn obj
 
   -- TODO: Name output file based on input file
   -- TODO: Generate Makefile fragment? Yes. Gen-C each module, Gen-makefile, make -f $exe.make $exe
