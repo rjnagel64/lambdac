ddb73dee338949e081b9201ba575c8eb64fb6165
Robert J Nagel <rjnagel64@gmail.com>
2022-04-04 10:51:31 -0500

    Display non-integer result values

--- rts/main.c
+++ rts/main.c
@@ -6,6 +6,91 @@
 #include "control.h"
 #include "panic.h"
 
+struct string_buf {
+    size_t len;
+    size_t capacity;
+    char *data;
+};
+
+struct string_buf *string_buf_new() {
+    struct string_buf *sb = malloc(sizeof(struct string_buf));
+    sb->len = 0;
+    sb->capacity = 8;
+    sb->data = malloc(sb->capacity * sizeof(char));
+    return sb;
+}
+
+void string_buf_destroy(struct string_buf *sb) {
+    free(sb->data);
+    free(sb);
+}
+
+void string_buf_push(struct string_buf *sb, const char *s) {
+    size_t len = strlen(s);
+    size_t capacity = sb->capacity;
+    while (sb->len + len + 1 > capacity) {
+        capacity *= 2;
+    }
+    if (capacity != sb->capacity) {
+        sb->data = realloc(sb->data, capacity);
+        sb->capacity = capacity;
+    }
+    memcpy(sb->data + sb->len, s, len);
+    sb->len += len;
+    sb->data[sb->len] = '\0';
+}
+
+// Render any value as a string.
+// Once I have a functioning IO system, this should probably be replaced with
+// whatever->string primops.
+void display_alloc(int prec, struct alloc_header *alloc, struct string_buf *sb) {
+    switch (alloc->type) {
+    case ALLOC_CLOSURE:
+        string_buf_push(sb, "<closure>");
+        break;
+    case ALLOC_CONST:
+        {
+        struct value *v = AS_VALUE(alloc);
+        char s[16];
+        sprintf(s, "%d", int32_value(v));
+        string_buf_push(sb, s);
+        }
+        break;
+    case ALLOC_PROD:
+        {
+        struct value *v = AS_VALUE(alloc);
+        string_buf_push(sb, "(");
+        display_alloc(0, AS_ALLOC(v->words[0]), sb);
+        string_buf_push(sb, ", ");
+        display_alloc(0, AS_ALLOC(v->words[1]), sb);
+        string_buf_push(sb, ")");
+        }
+        break;
+    case ALLOC_SUM:
+        {
+        struct sum *v = AS_SUM(alloc);
+        if (prec > 0) {
+            string_buf_push(sb, "(");
+        }
+        switch (v->discriminant) {
+        case 0:
+            string_buf_push(sb, "inl");
+            break;
+        case 1:
+            string_buf_push(sb, "inr");
+            break;
+        };
+        for (uint32_t i = 0; i < v->num_fields; i++) {
+            string_buf_push(sb, " ");
+            display_alloc(1, AS_ALLOC(v->words[i]), sb);
+        }
+        if (prec > 0) {
+            string_buf_push(sb, ")");
+        }
+        }
+        break;
+    }
+}
 
 // Provided by user.
 void program_entry(void);
@@ -31,12 +116,10 @@ int main(void) {
     free(next_step);
     // Display the result value.
     // Once I have a functioning IO system, this can go away.
-    if (result_value->type == ALLOC_CONST) {
-        int32_t result = int32_value(AS_VALUE(result_value));
-        printf("result = %d\n", result);
-    } else {
-        printf("FIXME: display values other than integers\n");
-    }
+    struct string_buf *sb = string_buf_new();
+    display_alloc(0, result_value, sb);
+    printf("result = %s\n", sb->data);
+    string_buf_destroy(sb);
 
     // Cleanup.
     destroy_locals();
