9da82df8bec08061c4aa5e4d1a7c319aeb723795
Robert J Nagel <rjnagel64@gmail.com>
2022-11-08 14:33:27 -0600

    Test impredicative instantiations

--- test_suite.py
+++ test_suite.py
@@ -78,6 +78,7 @@ standard_test("polystate", "(55, 55)")
 standard_test("rank2poly", "true")
 standard_test("foldr", "7")
 standard_test("polycase", "17")
+standard_test("impred_inst", "<closure>")
 
 for (test, out, err) in failed_tests:
     print(f"--- FAILED: {test} ---")
