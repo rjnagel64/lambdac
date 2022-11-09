8e983222a228b71bfed36bb85077c0c6a47167dc
Robert J Nagel <rjnagel64@gmail.com>
2022-11-09 10:40:28 -0600

    Apply hatchet to CC
    Remove the entirety of the old CC function, completely eliminate the
    "FieldsFor" boilerplate, make everything more concise and more obviously
    correct.

--- src/CC.hs
+++ src/CC.hs
@@ -29,20 +29,9 @@ module CC
   , pprintProgram
   ) where
 
-import qualified Data.Set as Set
-import Data.Set (Set)
-import qualified Data.Map as Map
-import Data.Map (Map)
-import Control.Monad.Reader
-import Control.Monad.Writer hiding (Sum)
-
-import Data.Function (on)
 import Data.List (intercalate)
-import Data.Bifunctor
-import Prelude hiding (abs, cos)
 
-import qualified CPS as K
-import CPS (TermK(..), FunDef(..), ContDef(..), AbsDef(..), ValueK(..), ArithK(..), CmpK(..))
+-- TODO: Eventually, this module should become essentially CC.IR
 
 -- Closure conversion:
 -- https://gist.github.com/jozefg/652f1d7407b7f0266ae9
@@ -54,6 +43,12 @@ import CPS (TermK(..), FunDef(..), ContDef(..), AbsDef(..), ValueK(..), ArithK(.
 --     f = <{a := a, b := b}, \x -> <{a := a, b := b, x = x}, \y -> a*x + b*y>>;
 --   in
 --     f 2 5
+
+-- Closure conversion is not lambda lifting.
+-- CC involves capturing the environment when a function is created (but the
+-- call site remains mostly the same), LL requires altering the call sites.
+-- (LL is O(n^3) or O(n^2), CC is less?)
+-- https://pages.github.ccs.neu.edu/jhemann/21SP-CS4400/FAQ/closure-conv/
 
 -- Note: [Closure Conversion and Lifting]
 -- After closure conversion, every lambda is annotated with its free variables.
@@ -74,6 +69,15 @@ import CPS (TermK(..), FunDef(..), ContDef(..), AbsDef(..), ValueK(..), ArithK(.
 -- (Also, this version tends to have lots of fst/snd from nested pairs. Could
 -- do n-ary tuples instead.)
 
+-- Idea: I could factor out the fv computation by first doing a annotation pass
+-- over the data, and then having 'cconv :: TermK EnvFields -> ConvM TermC'.
+-- This does get a bit messy with both TmVar/CoVar and Name being present,
+-- though, so I'll stick with the current approach.
+
+-- What does well-typed closure conversion look like?
+-- How are the values in a closure bound?
+
+
 -- The sort of a name can be determined from where it is bound.
 data Name = Name String Int
   deriving (Eq, Ord)
@@ -84,21 +88,11 @@ instance Show Name where
 prime :: Name -> Name
 prime (Name x i) = Name x (i+1)
 
--- TODO: Eliminate tmVar, coVar, and tyVar. They are not aware of the Context.
-tmVar :: K.TmVar -> Name
-tmVar (K.TmVar x i) = Name x i
-
-coVar :: K.CoVar -> Name
-coVar (K.CoVar k i) = Name k i
-
 data TyVar = TyVar String
   deriving (Eq, Ord)
 
 instance Show TyVar where
   show (TyVar aa) = aa
-
-tyVar :: K.TyVar -> TyVar
-tyVar (K.TyVar aa i) = TyVar (aa ++ show i)
 
 -- | 'Sort' is really a simplified form of type information.
 -- Value = int
@@ -136,20 +130,6 @@ instance Show TeleEntry where
   show (ValueTele s) = show s
   show (TypeTele aa) = '@' : show aa
 
-sortOf :: K.TypeK -> Sort
-sortOf (K.SumK _ _) = Sum
-sortOf K.BoolK = Boolean
-sortOf (K.ProdK t1 t2) = Pair (sortOf t1) (sortOf t2)
-sortOf K.UnitK = Unit
-sortOf K.IntK = Integer
-sortOf (K.ListK t) = List (sortOf t)
-sortOf (K.FunK ts ss) = Closure (map (ValueTele . sortOf) ts ++ map (ValueTele . coSortOf) ss)
-sortOf (K.TyVarOccK aa) = Alloc (tyVar aa)
-sortOf (K.AllK aas ss) = Closure (map (TypeTele . tyVar) aas ++ map (ValueTele . coSortOf) ss)
-
-coSortOf :: K.CoTypeK -> Sort
-coSortOf (K.ContK ss) = Closure (map (ValueTele . sortOf) ss)
-
 -- Closure conversion is bottom-up (to get flat closures) traversal that
 -- replaces free variables with references to an environment parameter.
 data TermC
@@ -173,12 +153,6 @@ data CaseKind
   = CaseBool
   | CaseSum Sort Sort
   | CaseList Sort
-
-caseKind :: K.TypeK -> CaseKind
-caseKind K.BoolK = CaseBool
-caseKind (K.SumK a b) = CaseSum (sortOf a) (sortOf b)
-caseKind (K.ListK a) = CaseList (sortOf a)
-caseKind _ = error "cannot perform case analysis on this type"
 
 -- | A 'BranchType' specifies the argument sorts expected by a case branch.
 newtype BranchType = BranchType [Sort]
@@ -253,325 +227,6 @@ data ValueC
   | BoolC Bool
   | EmptyC
   | ConsC Name Name
-
-
-data FreeOcc = FreeOcc { freeOccName :: Name, freeOccSort :: Sort }
-
-freeOcc :: FreeOcc -> (Name, Sort)
-freeOcc (FreeOcc x s) = (x, s)
-
-instance Eq FreeOcc where
-  (==) = (==) `on` freeOccName
-
-instance Ord FreeOcc where
-  compare = compare `on` freeOccName
-
--- I'm not entirely satisfied with the existence of 'FieldsFor'. It should be a
--- bottom-up traversal, just like the 'cconv' pass, but I end up separating it
--- out and repeatedly invoking it. (the time complexity is not correct.)
---
--- I think that the "proper" way to do it is with a Writer monad, and using
--- 'censor' to implement 'bindFields'? I might just keep it as part of the
--- return value, though.
-newtype FieldsFor = FieldsFor { runFieldsFor :: Context -> (Set FreeOcc, Set TyVar) }
-
-data Context
-  = Context {
-    ctxTms :: Map K.TmVar (Name, Sort)
-  , ctxCos :: Map K.CoVar (Name, Sort)
-  , ctxTys :: Map K.TyVar TyVar
-  }
-
-instance Semigroup FieldsFor where
-  fs <> fs' = FieldsFor $ \ctx -> runFieldsFor fs ctx <> runFieldsFor fs' ctx
-
-instance Monoid FieldsFor where
-  mempty = FieldsFor $ \_ -> (Set.empty, Set.empty)
-
-unitTm :: K.TmVar -> FieldsFor
-unitTm x = FieldsFor $ \ctx -> case Map.lookup x (ctxTms ctx) of
-  Nothing -> error ("unbound variable occurrence: " ++ show x ++ " not in " ++ show (ctxTms ctx))
-  Just (x', s) -> (Set.singleton (FreeOcc x' s), Set.empty)
-
-unitCo :: K.CoVar -> FieldsFor
-unitCo k = FieldsFor $ \ctx -> case Map.lookup k (ctxCos ctx) of
-  Nothing -> error ("unbound variable occurrence: " ++ show k ++ " not in " ++ show (ctxCos ctx))
-  Just (k', s) -> (Set.singleton (FreeOcc k' s), Set.empty)
-
-unitTy :: K.TyVar -> FieldsFor
-unitTy aa = FieldsFor $ \ctx -> case Map.lookup aa (ctxTys ctx) of
-  Nothing -> error ("unbound type variable occurrence: " ++ show aa ++ " not in " ++ show (ctxTys ctx))
-  Just aa' -> (Set.empty, Set.singleton aa')
-
-
-insertMany :: Ord k => [(k, v)] -> Map k v -> Map k v
-insertMany xs m = foldr (uncurry Map.insert) m xs
-
-bindTms :: [(K.TmVar, K.TypeK)] -> FieldsFor -> FieldsFor
-bindTms xs f = FieldsFor $ \ctx ->
-  let xs' = map (\b -> (fst b, bimap tmVar sortOf b)) xs in
-  let ctx' = ctx { ctxTms = insertMany xs' (ctxTms ctx) } in
-  let (fields, tyfields) = runFieldsFor f ctx' in
-  let bound = Set.fromList $ map (\ (_, (x, s)) -> FreeOcc x s) xs' in
-  (fields Set.\\ bound, tyfields)
-
-bindCos :: [(K.CoVar, K.CoTypeK)] -> FieldsFor -> FieldsFor
-bindCos ks f = FieldsFor $ \ctx ->
-  let ks' = map (\b -> (fst b, bimap coVar coSortOf b)) ks in
-  let ctx' = ctx { ctxCos = insertMany ks' (ctxCos ctx) } in
-  let (fields, tyfields) = runFieldsFor f ctx' in
-  let bound = Set.fromList $ map (\ (_, (k, s)) -> FreeOcc k s) ks' in
-  (fields Set.\\ bound, tyfields)
-
-bindTys :: [K.TyVar] -> FieldsFor -> FieldsFor
-bindTys aas f = FieldsFor $ \ctx ->
-  let aas' = map (\a -> (a, tyVar a)) aas in
-  let ctx' = ctx { ctxTys = insertMany aas' (ctxTys ctx) } in
-  let (fields, tyfields) = runFieldsFor f ctx' in
-  let bound = Set.fromList (map snd aas') in
-  (fields, tyfields Set.\\ bound)
-
-
-fieldsFor :: TermK a -> FieldsFor
-fieldsFor (LetFunK fs e) =
-  bindTms fs' $ foldMap (fieldsForFunDef) fs <> fieldsFor e
-  where fs' = [(f, K.FunK (map snd xs) (map snd ks)) | FunDef _ f xs ks _ <- fs]
-fieldsFor (LetContK ks e) =
-  bindCos ks' $ foldMap (fieldsForContDef) ks <> fieldsFor e
-  where ks' = [(k, K.ContK (map snd xs)) | ContDef _ k xs _ <- ks]
-fieldsFor (LetAbsK fs e) =
-  bindTms fs' $ foldMap (fieldsForAbsDef) fs <> fieldsFor e
-  where fs' = [(f, K.AllK as (map snd ks)) | AbsDef _ f as ks _ <- fs]
-fieldsFor (HaltK x) = unitTm x
-fieldsFor (JumpK k xs) = unitCo k <> foldMap unitTm xs
-fieldsFor (CallK f xs ks) = unitTm f <> foldMap unitTm xs <> foldMap unitCo ks
-fieldsFor (CaseK x _ ks) = unitTm x <> foldMap unitCo ks
-fieldsFor (InstK f ts ks) = unitTm f <> foldMap fieldsForTy ts <> foldMap unitCo ks
-fieldsFor (LetFstK x t y e) =
-  unitTm y <> fieldsForTy t <> bindTms [(x, t)] (fieldsFor e)
-fieldsFor (LetSndK x t y e) =
-  unitTm y <> fieldsForTy t <> bindTms [(x, t)] (fieldsFor e)
-fieldsFor (LetValK x t v e) =
-  fieldsForValue v <> fieldsForTy t <> bindTms [(x, t)] (fieldsFor e)
-fieldsFor (LetArithK x op e) = fieldsForArith op <> bindTms [(x, K.IntK)] (fieldsFor e)
-fieldsFor (LetNegateK x y e) = unitTm y <> bindTms [(x, K.IntK)] (fieldsFor e)
-fieldsFor (LetCompareK x cmp e) = fieldsForCmp cmp <> bindTms [(x, K.BoolK)] (fieldsFor e)
-
-fieldsForArith :: ArithK -> FieldsFor
-fieldsForArith (AddK x y) = unitTm x <> unitTm y
-fieldsForArith (SubK x y) = unitTm x <> unitTm y
-fieldsForArith (MulK x y) = unitTm x <> unitTm y
-
-fieldsForCmp :: CmpK -> FieldsFor
-fieldsForCmp (CmpEqK x y) = unitTm x <> unitTm y
-fieldsForCmp (CmpNeK x y) = unitTm x <> unitTm y
-fieldsForCmp (CmpLtK x y) = unitTm x <> unitTm y
-fieldsForCmp (CmpLeK x y) = unitTm x <> unitTm y
-fieldsForCmp (CmpGtK x y) = unitTm x <> unitTm y
-fieldsForCmp (CmpGeK x y) = unitTm x <> unitTm y
-
-fieldsForValue :: ValueK -> FieldsFor
-fieldsForValue (IntValK _) = mempty
-fieldsForValue (BoolValK _) = mempty
-fieldsForValue NilK = mempty
-fieldsForValue (PairK x y) = unitTm x <> unitTm y
-fieldsForValue (InlK x) = unitTm x
-fieldsForValue (InrK y) = unitTm y
-fieldsForValue EmptyK = mempty
-fieldsForValue (ConsK x y) = unitTm x <> unitTm y
-
-fieldsForFunDef :: FunDef a -> FieldsFor
-fieldsForFunDef (FunDef _ _f xs ks e) =
-  bindTms xs (bindCos ks (fieldsFor e)) <>
-  foldMap (fieldsForTy . snd) xs <>
-  foldMap (fieldsForCoTy . snd) ks
-
-fieldsForContDef :: ContDef a -> FieldsFor
-fieldsForContDef (ContDef _ _k xs e) =
-  bindTms xs (fieldsFor e) <>
-  foldMap (fieldsForTy . snd) xs
-
-fieldsForAbsDef :: AbsDef a -> FieldsFor
-fieldsForAbsDef (AbsDef _ _f as ks e) =
-  bindTys as $
-    foldMap (fieldsForCoTy . snd) ks <>
-    bindCos ks (fieldsFor e)
-
-fieldsForTy :: K.TypeK -> FieldsFor
-fieldsForTy K.UnitK = mempty
-fieldsForTy K.IntK = mempty
-fieldsForTy K.BoolK = mempty
-fieldsForTy (K.ProdK t s) = fieldsForTy t <> fieldsForTy s
-fieldsForTy (K.SumK t s) = fieldsForTy t <> fieldsForTy s
-fieldsForTy (K.ListK t) = fieldsForTy t
-fieldsForTy (K.FunK ts ss) = foldMap fieldsForTy ts <> foldMap fieldsForCoTy ss
-fieldsForTy (K.TyVarOccK aa) = unitTy aa
-fieldsForTy (K.AllK aas ss) = bindTys aas (foldMap fieldsForCoTy ss)
-
-fieldsForCoTy :: K.CoTypeK -> FieldsFor
-fieldsForCoTy (K.ContK ss) = foldMap fieldsForTy ss
-
--- | Compute the free type variables of a 'Sort'.
--- Kind of a hack; used to make sure that environments contain all the type
--- variables needed by their value fields.
-ftv :: Sort -> Set TyVar
-ftv (Alloc aa) = Set.singleton aa
-ftv (Closure tele) = foldr f Set.empty tele
-  where
-    f (ValueTele s) acc = ftv s <> acc
-    f (TypeTele aa) acc = Set.delete aa acc
-ftv Integer = Set.empty
-ftv Unit = Set.empty
-ftv Sum = Set.empty
-ftv Boolean = Set.empty
-ftv (Pair t s) = ftv t <> ftv s
-ftv (List s) = ftv s
-
--- TODO: The typing context for ConvM should be
--- '(Map K.TmVar (Name, Sort), Map K.CoVar (Name, Sort), Map K.TyVar TyVar)'
-newtype ConvM a = ConvM { runConvM :: Reader Context a }
-
-deriving newtype instance Functor ConvM
-deriving newtype instance Applicative ConvM
-deriving newtype instance Monad ConvM
-deriving newtype instance MonadReader Context ConvM
-
-runConv :: ConvM a -> a
-runConv = flip runReader emptyContext . runConvM
-  where emptyContext = Context Map.empty Map.empty Map.empty
-
-withTm :: K.TmVar -> K.TypeK -> ((Name, Sort) -> ConvM a) -> ConvM a
-withTm x t kont = local extend (kont (x', t'))
-  where
-    x' = tmVar x
-    t' = sortOf t
-    extend (Context tms cos tys) = Context (Map.insert x (x', t') tms) cos tys
-
-withTmBinds :: [(K.TmVar, K.TypeK)] -> ([(Name, Sort)] -> ConvM a) -> ConvM a
-withTmBinds xs kont = local extend (kont (map snd tmbinds))
-  where
-    tmbinds = map (\ (x, t) -> (x, (tmVar x, sortOf t))) xs
-    extend (Context tms cos tys) = Context (insertMany tmbinds tms) cos tys
-
-withCoBinds :: [(K.CoVar, K.CoTypeK)] -> ([(Name, Sort)] -> ConvM a) -> ConvM a
-withCoBinds ks kont = local extend (kont (map snd cobinds))
-  where
-    cobinds = map (\ (k, s) -> (k, (coVar k, coSortOf s))) ks
-    extend (Context tms cos tys) = Context tms (insertMany cobinds cos) tys
-
-withTyBinds :: [K.TyVar] -> ([TyVar] -> ConvM a) -> ConvM a
-withTyBinds as kont = local extend (kont (map snd tybinds))
-  where
-    tybinds = map (\a -> (a, tyVar a)) as
-    extend (Context tms cos tys) = Context tms cos (insertMany tybinds tys)
-
-
-cconvProgram :: TermK a -> TermC
-cconvProgram e = runConv (cconv e)
-
--- Idea: I could factor out the fieldsFor computation by doing a first
--- annotation pass over the data, and then having
--- 'cconv :: TermK EnvFields -> ConvM TermC'
--- This does get a bit messy with both TmVar/CoVar and Name being present,
--- though.
-cconv :: TermK a -> ConvM TermC
-cconv (LetFunK fs e) = do
-  let tmbinds = [(f, K.FunK (map snd xs) (map snd ks)) | FunDef _ f xs ks _ <- fs]
-  withTmBinds tmbinds $ \tmbinds' -> LetFunC <$> traverse cconvFunDef fs <*> cconv e
-cconv (LetContK ks e) = do
-  let cobinds = [(k, K.ContK (map snd xs)) | ContDef _ k xs _ <- ks]
-  withCoBinds cobinds $ \cobinds' -> LetContC <$> traverse cconvContDef ks <*> cconv e
-cconv (LetAbsK fs e) = do
-  let tmbinds = [(f, K.AllK as (map snd ks)) | AbsDef _ f as ks _ <- fs]
-  withTmBinds tmbinds $ \tmbinds' -> LetAbsC <$> traverse cconvAbsDef fs <*> cconv e
-cconv (HaltK x) = HaltC <$> cconvTmVar x
-cconv (JumpK k xs) = JumpC <$> cconvCoVar k <*> traverse cconvTmVar xs
-cconv (CallK f xs ks) = CallC <$> cconvTmVar f <*> traverse cconvTmVar xs <*> traverse cconvCoVar ks
-cconv (InstK f ts ks) = InstC <$> cconvTmVar f <*> pure (map sortOf ts) <*> traverse cconvCoVar ks
-cconv (CaseK x t ks) = CaseC <$> cconvTmVar x <*> pure (caseKind t) <*> traverse cconvCoVar ks
-cconv (LetFstK x t y e) = withTm x t $ \b -> LetFstC b <$> cconvTmVar y <*> cconv e
-cconv (LetSndK x t y e) = withTm x t $ \b -> LetSndC b <$> cconvTmVar y <*> cconv e
-cconv (LetValK x t v e) = withTm x t $ \b -> LetValC b <$> cconvValue v <*> cconv e
-cconv (LetArithK x op e) = withTm x K.IntK $ \b -> LetArithC b <$> cconvArith op <*> cconv e
-cconv (LetNegateK x y e) = withTm x K.IntK $ \b -> LetNegateC b <$> cconvTmVar y <*> cconv e
-cconv (LetCompareK x cmp e) = withTm x K.BoolK $ \b -> LetCompareC b <$> cconvCmp cmp <*> cconv e
-
-cconvFunDef :: FunDef a -> ConvM FunClosureDef
-cconvFunDef fun@(FunDef _ f xs ks e) = do
-  withTmBinds xs $ \tmbinds -> do
-    withCoBinds ks $ \cobinds -> do
-      env <- cconvEnvDef fieldsForFunDef fun
-      e' <- cconv e
-      pure (FunClosureDef (tmVar f) env tmbinds cobinds e')
-
-cconvContDef :: ContDef a -> ConvM ContClosureDef
-cconvContDef kont@(ContDef _ k xs e) = do
-  withTmBinds xs $ \tmbinds -> do
-    env <- cconvEnvDef fieldsForContDef kont
-    e' <- cconv e
-    pure (ContClosureDef (coVar k) env tmbinds e')
-
-cconvAbsDef :: AbsDef a -> ConvM AbsClosureDef
-cconvAbsDef abs@(AbsDef _ f as ks e) = do
-  withTyBinds as $ \tybinds -> do
-    withCoBinds ks $ \cobinds -> do
-      env <- cconvEnvDef fieldsForAbsDef abs
-      e' <- cconv e
-      pure (AbsClosureDef (tmVar f) env tybinds cobinds e')
-
-cconvEnvDef :: (a -> FieldsFor) -> a -> ConvM EnvDef
-cconvEnvDef fieldsForDef def = do
-  ctx <- ask
-  let (fields, tyfields) = runFieldsFor (fieldsForDef def) ctx
-  let tyfields' = foldr (\occ tys -> ftv (freeOccSort occ) <> tys) tyfields (Set.toList fields)
-  let env = EnvDef (Set.toList tyfields') (map freeOcc $ Set.toList fields)
-  pure env
-
-cconvTmVar :: K.TmVar -> ConvM Name
-cconvTmVar x = do
-  tms <- asks ctxTms
-  case Map.lookup x tms of
-    Nothing -> error ("variable not in scope: " ++ show x)
-    Just (x', _) -> pure x'
-
-cconvCoVar :: K.CoVar -> ConvM Name
-cconvCoVar k = do
-  cos <- asks ctxCos
-  case Map.lookup k cos of
-    Nothing -> error ("variable not in scope: " ++ show k)
-    Just (k', _) -> pure k'
-
-cconvValue :: ValueK -> ConvM ValueC
-cconvValue NilK = pure NilC
-cconvValue (PairK x y) = PairC <$> cconvTmVar x <*> cconvTmVar y
-cconvValue (IntValK i) = pure (IntC i)
-cconvValue (BoolValK b) = pure (BoolC b)
-cconvValue (InlK x) = InlC <$> cconvTmVar x
-cconvValue (InrK y) = InrC <$> cconvTmVar y
-cconvValue EmptyK = pure EmptyC
-cconvValue (ConsK x y) = ConsC <$> cconvTmVar x <*> cconvTmVar y
-
-cconvArith :: ArithK -> ConvM ArithC
-cconvArith (AddK x y) = AddC <$> cconvTmVar x <*> cconvTmVar y
-cconvArith (SubK x y) = SubC <$> cconvTmVar x <*> cconvTmVar y
-cconvArith (MulK x y) = MulC <$> cconvTmVar x <*> cconvTmVar y
-
-cconvCmp :: CmpK -> ConvM CmpC
-cconvCmp (CmpEqK x y) = EqC <$> cconvTmVar x <*> cconvTmVar y
-cconvCmp (CmpNeK x y) = NeC <$> cconvTmVar x <*> cconvTmVar y
-cconvCmp (CmpLtK x y) = LtC <$> cconvTmVar x <*> cconvTmVar y
-cconvCmp (CmpLeK x y) = LeC <$> cconvTmVar x <*> cconvTmVar y
-cconvCmp (CmpGtK x y) = GtC <$> cconvTmVar x <*> cconvTmVar y
-cconvCmp (CmpGeK x y) = GeC <$> cconvTmVar x <*> cconvTmVar y
-
--- What does well-typed closure conversion look like?
--- How are the values in a closure bound?
-
--- Closure conversion is not lambda lifting.
--- CC involves capturing the environment when a function is created (but the
--- call site remains mostly the same), LL requires altering the call sites.
--- (LL is O(n^3) or O(n^2), CC is less?)
--- https://pages.github.ccs.neu.edu/jhemann/21SP-CS4400/FAQ/closure-conv/
 
 
 indent :: Int -> String -> String
