36e8d07b1e2fc633168d7105ab8421b170e54c13
Robert J Nagel <rjnagel64@gmail.com>
2022-12-17 12:57:18 -0600

    CPS2: Restrict ObjCont to continuation variables

--- src/Experiments/CPS2.hs
+++ src/Experiments/CPS2.hs
@@ -27,38 +27,32 @@ data Term
 
 data KValue
   = KVar Name
-  -- I don't really like how functions are treated as atoms.
-  -- This can lead to redexes like (\x. x) 17 being left in.
-  -- ==> stratify a bit, re-introduce let-bindings for functions?
-  -- ==> reify MetaCont needs work, to hoist the contdef out of the jump
-  -- ==> (Hang on, a let-bound function still involves a redex, it's just hidden)
-  --
-  -- I guess what I want to say is that having a lambda is okay in argument
-  -- position, but I don't really want to have a lambda in function position,
-  -- because that's a redex that should be eliminated, right?
-  --
-  -- Or maybe, redexes in the source language should still be redexes in the
-  -- target language? So it's actually fine?
-  -- CPS should not introduce extra redexes, but redexes in the source language
-  -- are fine.
-  --
-  -- Also, 'letfun f x k = ... in f 17 halt' is a redex, just disguised slightly,
-  -- so my original doubt was incoherent.
-  --
-  -- I guess what I really want to say is that in my current IR, all functions
-  -- and continuations are named, from CPS.IR onwards.
-  --
-  -- This translation does not name functions or continuations. I need some way
-  -- to cope with that.
-  -- * I could have an immediate post-processing pass after CPS to clean this
-  -- up, but then I would have a mostly-redundant IR. (a nanopass to name
-  -- functions/conts in CPS)
-  --
-  -- * I could also make naming functions and continuations the responsibility
-  -- of CC. However, I currently plan to do most of my optimizations on CPS,
-  -- where it may be useful to have named fun/conts?
   | KLam Name Term
 
+-- In my current IR, all functions and continuations are named, from CPS.IR
+-- onwards.  This translation does not name functions or continuations. I need
+-- some way to cope with that.
+--
+-- * I could have an immediate post-processing pass after CPS to clean this
+--   up, but then I would have a mostly-redundant IR. (a nanopass to name
+--   functions/conts in CPS)
+--
+-- * I could also make naming functions and continuations the responsibility
+--   of CC. However, I currently plan to do most of my optimizations on CPS,
+--   where it may be useful to have named fun/conts?
+
+-- Realization: in the CPS translation algorithm, the continuation parameter
+-- describes the context in which the expression is being translated.
+--
+-- * A MetaCont is an arbitrary evaluation context.
+-- * An ObjCont (KVar "k") is a tail context.
+-- * ObjCont (KLam ...) doesn't really make much sense? It should be MetaCont
+--   to eliminate the redex when the continuation is applied.
+--
+-- I can also imagine an AppCont with an operand and an evaluation context /
+-- Cont for the result. Hypothetically, this could be used to uncurry
+-- applications. (The tricky part would be uncurrying the definitions, and then
+-- ensuring that only that many values are taken from an AppCont)
 
 
 -- Need at least Reader Int to implement fresh names.
@@ -69,22 +63,20 @@ type M a = Reader Int a
 
 
 data Cont
-  -- Hmm. Does it make sense to have 'ObjCont (KLam x e)', or could I restrict
-  -- this to 'ObjCont Name'?
-  = ObjCont KValue
+  = ObjCont Name
   | MetaCont (KValue -> M Term)
 
 apply :: Cont -> KValue -> M Term
-apply (ObjCont k) v = pure (KCall (KValue k) v)
+apply (ObjCont k) v = pure (KCall (KValue (KVar k)) v)
 apply (MetaCont k) v = k v
 
 reify :: Cont -> M KValue
-reify (ObjCont k) = pure k
+reify (ObjCont k) = pure (KVar k)
 reify (MetaCont k) = fresh $ \x -> KLam x <$> k (KVar x)
 
 cpsValue :: EValue -> M KValue
 cpsValue (EVar x) = pure (KVar x)
-cpsValue (ELam x e) = fresh $ \y -> KLam x . KValue . KLam y <$> cps e (ObjCont (KVar y))
+cpsValue (ELam x e) = fresh $ \y -> KLam x . KValue . KLam y <$> cps e (ObjCont y)
 
 cps :: Expr -> Cont -> M Term
 cps (EValue v) c = apply c =<< cpsValue v
@@ -98,4 +90,4 @@ cps (ELet x e1 e2) c =
 
 
 cpsMain :: Expr -> Term
-cpsMain e = runReader (cps e (ObjCont (KVar "halt"))) 0 -- just return the CPS'ed term.
+cpsMain e = runReader (cps e (ObjCont "halt")) 0 -- just return the CPS'ed term.
