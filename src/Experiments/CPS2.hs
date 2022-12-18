87b5eeeb4f8b3a2d766698d9b15d371c824201b5
Robert J Nagel <rjnagel64@gmail.com>
2022-12-17 21:56:29 -0600

    CPS2: Case expressions

--- src/Experiments/CPS2.hs
+++ src/Experiments/CPS2.hs
@@ -17,12 +17,16 @@ data Expr
   | ELam Name Expr
   | EApp Expr Expr
   | ELet Name Expr Expr
+  | EInl Expr
+  | EInr Expr
+  | ECase Expr (Name, Expr) (Name, Expr)
   deriving Show
 
 data Term
   = KHalt KValue
   | KCallFun KValue KValue KValue
   | KCallCont KValue KValue
+  | KCase KValue KValue KValue
   | KLet Name KValue Term
   deriving Show
 
@@ -30,6 +34,8 @@ data KValue
   = KVar Name
   | KFun Name Name Term
   | KCont Name Term
+  | KInl KValue
+  | KInr KValue
   deriving Show
 
 -- In my current IR, all functions and continuations are named, from CPS.IR
@@ -65,24 +71,50 @@ fresh k = reader (\i -> runReader (k ("v" ++ show i)) (i+1))
 type M a = Reader Int a
 
 
+-- |- c : a -o b
+-- when evaluation context is filled with value of type 'a', whole context has type b
 data Cont
+  -- if Γ |- k : a -> ! then |- (ObjCont k) : a -o b
+  -- Note that return type is polymorphic, because a jump does not return to
+  -- the original context.
   = ObjCont Name
+  -- if Γ |- v : CPS-Ty[a]  and  (e', b) <- f(v) then |- (MetaCont f) : a -o b
   | MetaCont (KValue -> M Term)
+  -- |- HaltCont : a -o a
   | HaltCont
 
+-- if
+-- |- c : a -o b  and  ε |- v : CPS-Ty[a]
+-- then
+-- (e', b) <- apply c (v, a)  and  ε |- e' OK
 apply :: Cont -> KValue -> M Term
 apply (ObjCont k) v = pure (KCallCont (KVar k) v)
 apply (MetaCont k) v = k v
 apply HaltCont v = pure (KHalt v)
 
+-- if
+-- |- c : a -o b
+-- then
+-- Γ |- reify c : (a) -> !
 reify :: Cont -> M KValue
 reify (ObjCont k) = pure (KVar k)
 reify (MetaCont k) = fresh $ \x -> KCont x <$> k (KVar x)
 reify HaltCont = fresh $ \x -> pure $ KCont x (KHalt (KVar x))
 
+-- if
+-- |- c : a -o b
+-- then
+-- (e', t) <- cps e c
+-- Γ |- e : t
+-- Γ' |- e' OK
+-- ???
 cps :: Expr -> Cont -> M Term
 cps (EVar x) c = apply c (KVar x)
-cps (ELam x e) c = apply c =<< (fresh $ \y -> KFun x y <$> cps e (ObjCont y))
+cps (ELam x e) c = do
+  -- Hmm. I might be able to restrict 'MetaCont' to accept a 'Name' if I
+  -- introduce a let-binding here for the function.
+  v <- fresh $ \k -> KFun x k <$> cps e (ObjCont k)
+  apply c v
 cps (EApp e1 e2) c =
   cps e1 $ MetaCont $ \v1 ->
     cps e2 $ MetaCont $ \v2 ->
@@ -90,10 +122,29 @@ cps (EApp e1 e2) c =
 cps (ELet x e1 e2) c =
   cps e1 $ MetaCont $ \v1 ->
     KLet x v1 <$> cps e2 c
+cps (EInl e) c =
+  cps e $ MetaCont $ \v -> apply c (KInl v)
+cps (EInr e) c =
+  cps e $ MetaCont $ \v -> apply c (KInr v)
+cps (ECase e (x, e1) (y, e2)) c =
+  cps e $ MetaCont $ \v ->
+    fresh $ \k -> do
+      k0 <- reify c -- continuation for the expression
+      -- Use a let-binding so that the continuation is not duplicated by each
+      -- branch.
+      KLet k k0 <$> (do
+        k1 <- KCont x <$> cps e1 (ObjCont k) -- left branch, cont x => [[e1]] k
+        k2 <- KCont y <$> cps e2 (ObjCont k) -- right branch, cont y => [[e2]] k
+        pure (KCase v k1 k2))
 
 
+-- Hmm. cpsMain :: Expr -> (Term, Type)
+-- if (e', t) = cpsMain e then
+-- ε |- e : t  and  ε |- e' OK
+--
+-- The type of cps should be a strengthening of this statement.
 cpsMain :: Expr -> Term
-cpsMain e = runReader (cps e HaltCont) 0 -- just return the CPS'ed term.
+cpsMain e = runReader (cps e HaltCont) 0
 
 main :: IO ()
 main = do
