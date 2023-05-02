05c886d69e37da6d1dba65467afd52e60be9a712
Robert J Nagel <rjnagel64@gmail.com>
2023-05-01 21:35:58 -0500

    WIP: Lowering

--- src/Lower.hs
+++ src/Lower.hs
@@ -1,6 +1,6 @@
 
 module Lower
-    (
+    ( lowerProgram
     ) where
 
 import qualified Hoist.IR as H
@@ -13,11 +13,14 @@ import Data.Map (Map)
 
 
 
-data Program = Program
+data Program = Program () Term
 
 data Term
   = Open ThunkType NameRef [ClosureArg]
-  | Halt NameRef
+  | Halt Type NameRef
+  -- probably need ThunkType here. Also, ctorDownCast, ctorArgCasts, and ctorDiscriminant
+  -- Maybe have Emit do lookup on environment keyed by (TyCon, Ctor)?
+  -- (Still a lookup, but a simple one on a top-level env.)
   | Case NameRef [(Ctor, NameRef)]
 
   | LetValue (Name, Type) Value Term
@@ -42,6 +45,7 @@ data CtorApp
   | CtorApp Ctor [NameRef]
 
 data Projection = ProjectFst | ProjectSnd
+
 data ClosureArg = ValueArg NameRef | TypeArg Type
 
 data ClosureAlloc = ClosureAlloc (Name, Type) ()
@@ -77,7 +81,7 @@ deriving newtype instance Applicative M
 deriving newtype instance Monad M
 deriving newtype instance MonadReader LowerEnv M
 
-data LowerEnv = LowerEnv { envThunkTypes :: Map H.Name ThunkType }
+data LowerEnv = LowerEnv { envThunkTypes :: Map H.Name ThunkType, envNames :: Map H.Name NameRef }
 
 
 lookupThunkType :: H.Name -> M ThunkType
@@ -87,17 +91,30 @@ lookupThunkType x = do
     Nothing -> error "Name without a thunk type"
     Just ty -> pure ty
 
+runM :: M a -> a
+runM m = flip runReader emptyEnv $ getM m
+  where
+    emptyEnv = LowerEnv { envThunkTypes = Map.empty, envNames = Map.empty }
 
-lowerProgram :: H.Program -> M Program
-lowerProgram (H.Program ds e) = pure Program
+
+lowerProgram :: H.Program -> Program
+lowerProgram (H.Program ds e) = runM $ Program <$> pure () <*> lowerTerm e
 
 lowerTerm :: H.TermH -> M Term
-lowerTerm _ = error "Not implemented: lowerTerm"
--- lowerTerm (H.OpenH cl args) =
---   Open <$> lookupThunkType cl <*> lowerName cl <*> traverse lowerClosureArg args
+lowerTerm (H.OpenH cl args) =
+  Open <$> lookupThunkType cl <*> lowerName cl <*> traverse lowerClosureArg args
 
--- lowerName :: H.Name -> M NameRef
--- lowerName x = _
+lowerName :: H.Name -> M NameRef
+lowerName x = do
+  env <- asks envNames
+  case Map.lookup x env of
+    Nothing -> error "Name not in scope"
+    Just x' -> pure x'
 
--- lowerClosureArg :: H.ClosureArg -> M ClosureArg
--- lowerClosureArg arg = _
+lowerClosureArg :: H.ClosureArg -> M ClosureArg
+lowerClosureArg (H.ValueArg x) = ValueArg <$> lowerName x
+lowerClosureArg (H.TypeArg s) = TypeArg <$> lowerType s
+
+
+lowerType :: H.Sort -> M Type
+lowerType H.IntegerH = pure TyInt64
