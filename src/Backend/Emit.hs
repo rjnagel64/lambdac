0537a110233f2b17a7e86bdceb92c8fb822da1c7
Robert J Nagel <rjnagel64@gmail.com>
2023-07-28 21:54:20 -0500

    Split Backend.IR from Backend.Lower

--- src/Backend/Emit.hs
+++ src/Backend/Emit.hs
@@ -13,7 +13,7 @@ import Data.Map (Map)
 import qualified Data.Set as Set
 import Data.Set (Set)
 
-import Backend.Lower
+import Backend.IR
 
 -- TODO: Something smarter than string and list concatenation.
 -- builders? text? environment?
